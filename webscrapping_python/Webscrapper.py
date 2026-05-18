951f9bbae3f37f9021f397cb4441d0c50f684da2
GOPIKA RAJ <159898289+Raj-Gopika@users.noreply.github.com>
2026-05-18 12:05:36 +0530

    Add files via upload

--- webscrapping_python/Webscrapper.py
+++ webscrapping_python/Webscrapper.py
@@ -1,1 +1,27 @@
-#Web scrapping with python
+# Web scrapping with python
+
+#----------------------------------------------#
+# Scrape financial/business news website
+# Extract:
+    # headlines
+    # timestamps
+    # categories
+    # links
+# Filter based on keywords
+# Generate clean HTML email report
+# Automatically send email daily
+
+#---------------------------------------------------#
+
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import datetime
+
+now = datetime.datetime.now()
+
+# content body placeholder
+content = ''
